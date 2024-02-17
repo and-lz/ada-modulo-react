f26c7b82fa4b5cb23a675c49318b39088ab183f5
André Luiz <abezerra.meiuca@veloe.com.br>
2024-02-16 22:46:32 -0300

    Remove conteudo nãO usado

--- src/components/Rotas.tsx
+++ src/components/Rotas.tsx
@@ -2,15 +2,12 @@ import PaginaCardapioRestaurante from "../pages/PaginaCardapioRestaurante";
 import PaginaRestaurantes from "../pages/PaginaRestaurantes";
 
 import { Route, Routes } from "react-router-dom";
-import Detalhes from "./Detalhes";
 
 function Rotas() {
   return (
     // Qual é o caminho, e o que será exibido naquele caminho
     <Routes>
       <Route path="/" element={<PaginaRestaurantes />} />
-      <Route path="/:id/:categoria/detalhes" element={<Detalhes />} />
-      <Route path="/busca" element={<Detalhes />} />
       <Route path="/:slug/cardapio" element={<PaginaCardapioRestaurante />} />
     </Routes>
   );
