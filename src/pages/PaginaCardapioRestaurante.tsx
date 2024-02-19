a700ed6d3f9abfbe0a57f0a7a3a73408891a01f5
André Luiz <abezerra.meiuca@veloe.com.br>
2024-02-18 21:32:52 -0300

    Adiciona busca nas páginas

--- src/pages/PaginaCardapioRestaurante.tsx
+++ src/pages/PaginaCardapioRestaurante.tsx
@@ -1,13 +1,19 @@
 import { Link, useParams } from "react-router-dom";
 import restaurantes from "../data/restaurantsAndItems.json";
 import CaixaItemRestaurante from "../components/CaixaItemRestaurante";
+import { useState } from "react";
 
 function PaginaCardapioRestaurante() {
   const { slug: idDaUrl } = useParams();
+  const [busca, definirBusca] = useState("");
 
   const restaurante = restaurantes.filter(
     (restaurante) => restaurante.slug === idDaUrl
   )[0];
+
+  const listaItens = restaurante.cardapio.filter((itemCardapio) =>
+    itemCardapio.nome.toLowerCase().includes(busca.toLowerCase())
+  );
 
   return (
     <section>
@@ -17,6 +23,8 @@ function PaginaCardapioRestaurante() {
       <input
         type="text"
         id="search"
+        value={busca}
+        onChange={(e) => definirBusca(e.target.value)}
         placeholder="Pesquisar pelo nome do item ou descrição..."
       />
       <label htmlFor="search">🔎</label>
@@ -25,10 +33,8 @@ function PaginaCardapioRestaurante() {
         role="list"
         style={{ "--max": "300px" } as React.CSSProperties}
       >
-        {restaurante.cardapio.map((item) => (
-          <>
-            <CaixaItemRestaurante item={item} />
-          </>
+        {listaItens.map((item) => (
+          <CaixaItemRestaurante item={item} />
         ))}
       </ul>
     </section>
--- src/pages/PaginaRestaurantes.tsx
+++ src/pages/PaginaRestaurantes.tsx
@@ -1,7 +1,20 @@
-import CaixaRestaurante from "../components/CaixaRestaurante";
+import { useState } from "react";
+import CaixaRestaurante, { Restaurante } from "../components/CaixaRestaurante";
 import restaurantes from "../data/restaurantsAndItems.json";
 
 function PaginaRestaurantes() {
+  const [busca, definirBusca] = useState("");
+
+  function buscar(restaurante: Restaurante) {
+    return (
+      restaurante.nome.toLowerCase().includes(busca.toLowerCase()) ||
+      restaurante.categoria.toLowerCase().includes(busca.toLowerCase()) ||
+      restaurante.descricao.toLowerCase().includes(busca.toLowerCase())
+    );
+  }
+
+  const listaRestaurantes = restaurantes.filter(buscar);
+
   return (
     <section>
       <h1>Restaurantes</h1>
@@ -9,10 +22,12 @@ function PaginaRestaurantes() {
         type="text"
         placeholder="Pesquisar pelo nome do restaurante, categoria ou descrição."
         id="search"
+        onChange={(e) => definirBusca(e.target.value)}
+        value={busca}
       />
       <label htmlFor="search">🔎</label>
       <ul className="grid" role="list">
-        {restaurantes.map((restaurante) => (
+        {listaRestaurantes.map((restaurante) => (
           <CaixaRestaurante key={restaurante.slug} restaurante={restaurante} />
         ))}
       </ul>
